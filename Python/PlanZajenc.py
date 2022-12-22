5ab46db168658ff8d9306b0a9e5f31e34b7f332e
M4tof <krzysztfmanczak@gmail.com>
2022-12-22 20:37:18 +0100

    Final 4 2day

--- Python/PlanZajenc.py
+++
@@ -1,3 +0,0 @@
-ane=[['Adam', 'Wata', 'Szachy:5', 'Skoki:7', 'Wojna:3'], ['Gabriel', 'Mucha', 'Wojna:3', 'Szachy:0', 'Skoki:9']]
-for g in range(len(ane)):
-    print(ane[g][2:len(ane[0])])
--- Python/Test.py
+++ Python/Test.py
@@ -1,6 +1,17 @@
-mat=[['A','C'],['D','C'],['A','A'],['D','A']]
+def magick(a):
+    a=str(a)
+    if len(a)<=2:
+        return True
+    else:
+        flag_rising=0
+        flag_droping=0
+        for i in range(1,len(a)):
+            if int(a[i-1])<int(a[i]):
+                flag_rising=1
+                if flag_rising==1 and flag_droping==1:
+                    return False
+            if int(a[i-1])>int(a[i]):
+                flag_droping=1
+    return True
 
-mat=sorted(mat, key=lambda x:x[1])[::-1]
-mat=sorted(mat,key=lambda x:x[0])
-
-print(mat)
+print(magick(2321))
