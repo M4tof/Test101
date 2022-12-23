670d69cb04e5c743aabf12d22284348d1fa185b8
M4tof_PC <krzysztfmanczak@gmail.com>
2022-12-23 20:08:58 +0100

    20.08

--- Python/PlanZajenc.py
+++ Python/PlanZajenc.py
@@ -1,10 +1,53 @@
 N=input().split()
-H=int(N[1])
+M=int(N[1])
 N=int(N[0])
 
-Trees=[]
-for ind in range(N):
-    Trees.append(list(map(int,input().split())))
+def RowMov(R,N):    #ROW NUMBER
+    L=R[:]
+    for g in range(len(R)):
+        L[g]=R[((g-N)%(len(R)))]
+    return L
 
-Trees=sorted(Trees,key=lambda x:x[0])
-print(Trees)
+def obrot(a): 
+    mat2=[]
+    for n in range(len(a[0])):
+        temptab=[]
+        for n2 in range(len(a)):
+            temptab.append(a[n2][n])
+        mat2.append(temptab)
+    return mat2
+
+mat1=[]
+matf=[]
+for i in range(N):
+    mat1.append(list(map(int,input().split())))
+for j in range(N):
+    matf.append(list(map(int,input().split())))
+
+if mat1==matf:
+    print("TAK")
+    quit()
+
+
+mat3=mat1[:]
+for y in range(N):
+    for z in range(1,N-1):
+        mat4=mat3[:]
+        mat4[y]=RowMov(mat3[y],z)
+        if mat4==matf:
+            print("TAK")
+            quit()
+
+mat5=obrot(matf)
+mat3=obrot(mat1[:])
+
+for y in range(M):
+    for z in range(1,M-1):
+        mat4=mat3[:]
+        mat4[y]=RowMov(mat3[y],z)
+        print(mat4)
+        if mat4==mat5:
+            print("TAK")
+            quit()
+
+print("NIE")
--- Python/Test.py
+++ Python/Test.py
@@ -1,9 +1,10 @@
-s = input()
-#print(sum(s.count(x)%2 for x in set(s)) < 2)
-print(sum( s.count( int(s) )%x for x in range( 2,int(s) ) ) ==0)
+v=[1,2,3,4,5,6,7,8,9]
 
+def RowMov(R,N):    #ROW NUMBER
+    L=R[:]
+    for g in range(len(R)):
+        L[g]=R[((g-N)%(len(R)))]
+    return L
 
-#for i in range(2,int(s)):
-    #if int(s)%i==0:
-        #print(False)
-        
+v=RowMov(v,len(v))
+print(v)
