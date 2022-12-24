cc2758a82062c236c2e952046c009a38a8a3cb89
M4tof_PC <krzysztfmanczak@gmail.com>
2022-12-24 11:48:16 +0100

    pomoc chemików

--- Python/PlanZajenc.py
+++ Python/PlanZajenc.py
@@ -1,53 +1,12 @@
-N=input().split()
-M=int(N[1])
-N=int(N[0])
+Butelki=[]
+for ind in range(int(input())):
+    A=input.split()
+    Butelki.append(A)
 
-def RowMov(R,N):    #ROW NUMBER
-    L=R[:]
-    for g in range(len(R)):
-        L[g]=R[((g-N)%(len(R)))]
-    return L
+Butelki=sorted(Butelki)
 
-def obrot(a): 
-    mat2=[]
-    for n in range(len(a[0])):
-        temptab=[]
-        for n2 in range(len(a)):
-            temptab.append(a[n2][n])
-        mat2.append(temptab)
-    return mat2
-
-mat1=[]
-matf=[]
-for i in range(N):
-    mat1.append(list(map(int,input().split())))
-for j in range(N):
-    matf.append(list(map(int,input().split())))
-
-if mat1==matf:
-    print("TAK")
-    quit()
-
-
-mat3=mat1[:]
-for y in range(N):
-    for z in range(1,N-1):
-        mat4=mat3[:]
-        mat4[y]=RowMov(mat3[y],z)
-        if mat4==matf:
-            print("TAK")
-            quit()
-
-mat5=obrot(matf)
-mat3=obrot(mat1[:])
-
-for y in range(M):
-    for z in range(1,M-1):
-        mat4=mat3[:]
-        mat4[y]=RowMov(mat3[y],z)
-        print(mat4)
-        if mat4==mat5:
-            print("TAK")
-            quit()
-
-print("NIE")
+for jnd in range(int(input())):
+    F=input().split()
+    From=F[0]
+    To=F[1]
+    
--- Python/Test.py
+++ Python/Test.py
@@ -1,10 +1,31 @@
-v=[1,2,3,4,5,6,7,8,9]
+v1=['A',3,1,5]
+v2=['B',0,2,4]      #Nazwa , sok,woda,pojemnosc
+v3=['C',2,0,4]
 
-def RowMov(R,N):    #ROW NUMBER
-    L=R[:]
-    for g in range(len(R)):
-        L[g]=R[((g-N)%(len(R)))]
-    return L
+def przelej(x,y): #from to
+    Can_Take=y[3]-y[1]-y[2]
+    Can_Provide = x[1]+x[2]
+    if Can_Take >= Can_Provide:
+        Will_do=Can_Provide
+    else:
+        Will_do=Can_Take
+    Stosunek=0
+    try:
+        Stosunek=x[1]/(x[2]+x[1])
+    except:
+        pass
 
-v=RowMov(v,len(v))
-print(v)
+    y[1]=y[1]+ ( Will_do*Stosunek)
+    y[2]=y[2] +(Will_do*(1-Stosunek))
+
+    x[1]=x[1]-(Will_do*Stosunek)
+    x[2]=x[2]-(Will_do*(1-Stosunek))
+    return [x,y]
+
+
+v2,v3=przelej(v2,v3)
+v3,v1=przelej(v3,v1)
+
+print(v1)
+print(v2)
+print(v3)
